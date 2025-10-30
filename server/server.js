ee980d3a80d0f11f498eb58e3dc9a32345808195
Anurag Dubey <mr.anurag9628@gmail.com>
2025-10-30 10:02:49 +0530

    Auth Routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import { connect } from "mongoose";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,9 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+//API Endpoints
 app.get("/", (req, res) => {
   res.send("API server Working");
 });
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
